504c87bb6c71e9b3ef9912d0c2841e68f6bb6478
Justyna Medrala-Sowa <jsowa@jsrv04>
2026-01-20 15:00:56 +0100

    Initial commit

--- run.C
+++ run.C
@@ -3,11 +3,7 @@
 
 void run()
 {
-    gROOT->ProcessLine(".L selector_3g.C++g");    
-
-    // Tworzymy obiekt Selector3g
     Selector3g s("/data/4/users/jsowa/data/Run10/MC/2025_10_16-23_08_22.ntu.root", "output.root", true);
-
-    // Uruchamiamy analizę
+    
     s.runAnalysis();
 }
